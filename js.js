1eef27aeab5fe6772d570caf6d6001f1bc058a6e
Milly <millylin@gmail.com>
2023-04-24 20:03:16 +0800

    Update js.js

--- js.js
+++ js.js
@@ -8,16 +8,16 @@ document.addEventListener('onResult', e => {
     console.log(e.detail.result)
       //顯示自訂結果的訊息框
   		document.getElementById("myOverlay").style.display = "block";
-			document.getElementById('result').innerHTML = e.detail.result
-			document.getElementById("myOverlay").classList.remove("fadeOut");
-			document.getElementById("myOverlay").classList.add("fadeIn");
+		document.getElementById('result').innerHTML = e.detail.result
+		document.getElementById("myOverlay").classList.remove("fadeOut");
+		document.getElementById("myOverlay").classList.add("fadeIn");
       //播放音效
-			document.getElementById("myAudio").play();
+		document.getElementById("myAudio").play();
       //設定六秒後訊息框自動消失
-			setTimeout(function(){
-				document.getElementById("myOverlay").style.display = "none";
-				document.getElementById("myOverlay").classList.remove("fadeIn");
-				document.getElementById("myOverlay").classList.add("fadeOut");
-			}, 6000);
+		setTimeout(function(){
+			document.getElementById("myOverlay").style.display = "none";
+			document.getElementById("myOverlay").classList.remove("fadeIn");
+			document.getElementById("myOverlay").classList.add("fadeOut");
+		}, 6000);
       // 改寫者 Milly Lin 2023.04
 })
